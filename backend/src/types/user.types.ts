483c08a0e06d446976ffb810c13f0dedfc9387a6
Krish Makhijani <makhijanikrish001@gmail.com>
2024-12-07 03:26:31 +0530

    Added Login Zod

--- backend/src/types/user.types.ts
+++ backend/src/types/user.types.ts
@@ -13,4 +13,9 @@ const verifyOtpSchema = z.object({
     otp: z.string().length(6, 'OTP must be 6 characters')
 })
 
-export { signupSchema, verifyOtpSchema }
+const loginSchema = z.object({
+    email: z.string().email('Invalid email format'),
+    password: z.string().min(6, 'Password must be at least 6 characters')
+})
+
+export { signupSchema, verifyOtpSchema, loginSchema }
